b2bc297f9179c6c11b472c153aaad6291d2f90fb
Dovguchic Evgeniy <dovgevgen@gmail.com>
2018-02-28 09:31:03 +0300

    blia

--- FansPen/Program.cs
+++ FansPen/Program.cs
@@ -19,25 +19,31 @@ namespace FansPen.Web
     {
         public static void Main(string[] args)
         {
-            //BuildWebHost(args).Run();
-            var host = BuildWebHost(args);
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
-                    var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
-                    var t = RoleInitializer.InitializeAsync(userManager, rolesManager);
-                    t.Wait();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while seeding the database.");
-                }
-            }
-            host.Run();
+            ////BuildWebHost(args).Run();
+            //var host = BuildWebHost(args);
+            ////using (var scope = host.Services.CreateScope())
+            ////{
+            ////    var services = scope.ServiceProvider;
+            ////    try
+            ////    {
+            ////        var userManager = services.GetRequiredService<UserManager<ApplicationUser>>();
+            ////        var rolesManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+            ////        var t = RoleInitializer.InitializeAsync(userManager, rolesManager);
+            ////        t.Wait();
+            ////    }
+            ////    catch (Exception ex)
+            ////    {
+            ////        var logger = services.GetRequiredService<ILogger<Program>>();
+            ////        logger.LogError(ex, "An error occurred while seeding the database.");
+            ////    }
+            ////}
+            //host.Run();
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseIISIntegration()
+                .UseSetting("detailedErrors", "true")
+                .CaptureStartupErrors(true)
+                .Build().Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
